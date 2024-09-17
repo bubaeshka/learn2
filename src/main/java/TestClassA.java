3157a86cdde6e15499c4b46b3ed9fa66a21cd825
bubaeshka <62142029+bubaeshka@users.noreply.github.com>
2024-09-17 22:01:21 +0700

    start commit

---
+++ src/main/java/TestClassA.java
@@ -0,0 +1,33 @@
+public class TestClassA {
+    private int a;
+    private String b;
+
+    public TestClassA(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "TestClassA{" +
+                "a=" + a +
+                ", b='" + b + '\'' +
+                '}';
+    }
+}
